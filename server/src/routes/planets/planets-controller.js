dee5724f6d7b47720b10ffef72d256da08ace76f
David Perez <david.ezequiel.pere@gmail.com>
2023-02-10 12:00:51 -0300

    First endpoint, planets, implemented.

---
+++ server/src/routes/planets/planets-controller.js
@@ -0,0 +1,13 @@
+const planets = require('../../models/planets-model')
+
+function getAllPlanets(req, res) {
+   return res.status(200).json(planets) 
+  /*  este return no lo usa express pero, (y esto nolo entiendo
+   bien )"Es para que asegurarnos que la funcion se ejecute 
+   una sola vez, porque sino express ya tiene los headers alocados...
+   o algo asi..." */
+}
+
+module.exports = {
+    getAllPlanets,
+}
