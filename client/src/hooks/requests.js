310ece199968e53ef7b324f7d42072f15fd66c51
David Perez <david.ezequiel.pere@gmail.com>
2023-02-11 17:42:02 -0300

    Implemented an unified package.json commands to run the whole project. #devops

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -2,7 +2,7 @@ const API_URL = 'http://localhost:8000'
 
 async function httpGetPlanets() {
   // TODO: Once API is ready.
-  const response = await fetch(`${API_URL}'/planets'`)
+  const response = await fetch(`${API_URL}/planets`)
   // Load planets and return as JSON.
   return await response.json()
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,23 @@
+//Imports
+
 const http = require('http')
 
 const app = require('./app')
 
+const { loadPlanetsData } = require('./models/planets-model')
+
 const PORT = process.env.PORT || 8000
+
+//Server setup
 
 const server = http.createServer(app)
 
-server.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}...`)
-})
+async function startServer(){
+    await loadPlanetsData()
 
+    server.listen(PORT, () => {
+        console.log(`Listening on port: ${PORT}...`)
+    })
+}
 
+startServer()
