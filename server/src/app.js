a4bc3dc5dd6d4d624a3e9fcb6cbc13d6e0ba308d
David Perez <david.ezequiel.pere@gmail.com>
2023-02-13 18:31:47 -0300

    * added to the / app.use req res

--- server/src/app.js
+++ server/src/app.js
@@ -29,8 +29,8 @@ app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(planetsRouter)
 app.use(launchesRouter)
 
-/* app.use('/',(req, res) =>{
+app.use('/*',(req, res) =>{
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
-}) */
+})
 
 module.exports = app
