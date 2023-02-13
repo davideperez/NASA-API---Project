08dc0e8dda081f3d65fb4ba342da9843c69aae48
David Perez <david.ezequiel.pere@gmail.com>
2023-02-13 13:20:52 -0300

    Logs with Morgan implemented

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,24 @@
+//imports - buildin
 const express = require('express')
 const path = require('path')
+//imports - 3rdparties
 const cors = require('cors')
+const morgan = require('morgan')
 
+//imports - custom
 const planetsRouter = require('./routes/planets/planets-router')
 
+//App setup
 const app = express()  
+
+//Middleware
 
 app.use(cors({
     origin: 'http://localhost:3000',
 }))
+
+app.use(morgan('combined'))
+
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
