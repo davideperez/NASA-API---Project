6d6067064af83121a91dea87eb3ce5bd4a53d478
David Perez <david.ezequiel.pere@gmail.com>
2023-02-14 12:22:24 -0300

    implemented the POST method

--- server/src/models/launches-model.js
+++ server/src/models/launches-model.js
@@ -1,12 +1,14 @@
 const launches = new Map()
 
+const latestFlightNumber = 100
+
 const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
     rocket: 'Explore IS1',
     launchDate: new Date('December 27, 2030'),
     destination: 'Keppler 442 b',
-    customer: ['ZTM', 'NASA'],
+    customers: ['ZTM', 'NASA'],
     upcoming: true,
     success: true
 }
@@ -17,6 +19,16 @@ function getAllLaunches() {
     return Array.from(launches.values())
 }
 
+function addNewLaunch(launch){
+    launches.set(latestFlightNumber, Object.assign(launch, {
+        success: true,
+        upcoming: true,
+        customers: ['David', 'Yamila'],
+        flightNumber: latestFlightNumber
+    }))
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
 }
