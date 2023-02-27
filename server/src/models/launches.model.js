f2cb5fc44cb0343829529b2c554eb7d6ada501a7
David Perez <david.ezequiel.pere@gmail.com>
2023-02-26 22:42:17 -0300

    Implemented the scheduleLaunch function as the former addLaunch but connected to MongoDB instead of to the local memory array.

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,9 +1,7 @@
-const launchesDataBase = require('./launches.mongo')
-const planets = require('./planets.mongo')
+const launchesDataBase = require('./launches.mongo');
+const planets = require('./planets.mongo');
 
-//const launches = new Map();
-
-let latestFlightNumber = 100;
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launch = {
     flightNumber: 100,
@@ -19,59 +17,90 @@ const launch = {
 saveLaunch(launch);
 
 function existsLaunchWithId(launchId) {
-    return launches.has(launchId)
+    // CORE: checcks
+    return launches.has(launchId);
 };
 
+async function getLatestFlightNumber() {
+    
+    // CORE Process: DB QUERY, sorts de db and fetches the id of the last input. 
+    const latestLunch = await launchesDataBase.findOne().sort('-flightNumber');
+    
+    // EXCEPTION: When there are no launches in the db.
+    if(!latestLunch) {
+        return DEFAULT_FLIGHT_NUMBER
+    }
+    
+    // RETURNS latest flightnumber.
+    return latestLunch.flightNumber;
+}
+
 async function getAllLaunches() {
-    //{} using an empty object mongo know we want all the entrys. The second parameter is the projection
-    // object. Were we define which properties dont want in our object, such as the mongodb id or version ob the
-    //  object.
+    /* {} using an empty object in .find({},{}) mongo know we want all the entrys. The second object parameter
+     is the projection object. There you can define which properties dont want in our object, 
+     such as the mongodb id or version ob the object. */
+
     return await launchesDataBase
         .find({}, {
             '_id': 0,
-            '__v': 0
-    })
+            '__v': 0,
+    });
 };
 
+/////////////////////////////////////
+// Sube una entrada a la db de mongo
+/////////////////////////////////////
+
 async function saveLaunch(launch) {
+    // VALIDACION de que existe el planeta del launch en la db de planetas.
     const planet = await planets.findOne({
         keplerName: launch.target,
     });
 
+    // Se implementa el ERROR en caso de que el planeta target no existiese.
     if (!planet) {
         throw new Error('No matching planets was found.');
     };    
 
+    // CORE de la funcion: se inserta el launch a la db en mongoDB.
     await launchesDataBase.updateOne({
         flightNumber: launch.flightNumber,
     }, launch, {
         upsert: true,
     });
-};
+}; 
 
-function addNewLaunch(launch){
-    latestFlightNumber++
-    launches.set(
-        latestFlightNumber,
-        Object.assign(launch, {
-            success: true,
-            upcoming: true,
-            customers: ['David', 'Yamila'],
-            flightNumber: latestFlightNumber
-    }));
-};
+//////////////////////////////////////////////////
+// Suma un documento json a la a la db de mongo.
+/////////////////////////////////////////////////
+
+async function scheduleNewLaunch(launch) {
+    // CALCULO: se calcula el id de los lanzamientos incrementando en 1 el id del ultimo lanzamiento. 
+    const newFlightNumber = await getLatestFlightNumber() + 1;
+
+    //CORE: Se construyen, aqui en el backend, los campos fijos del objeto launch, el resto los camos ya los asigno el front. 
+    const newLaunch = Object.assign(launch, {
+        success: true,
+        upcoming: true,
+        customers: ['David', 'Yamila'],
+        flightNumber: newFlightNumber,
+    })
+
+    await saveLaunch(newLaunch)
+}
+
 
 function abortsLaunchById(launchId) {
     const aborted = launches.get(launchId)
-    aborted.upcoming = false
-    aborted.success = false
+    aborted.upcoming = false;
+    aborted.success = false;
     
-    return aborted
+    return aborted;
 };
 
 module.exports = {
     existsLaunchWithId,
     getAllLaunches,
-    addNewLaunch,
+    scheduleNewLaunch,
     abortsLaunchById
 };
